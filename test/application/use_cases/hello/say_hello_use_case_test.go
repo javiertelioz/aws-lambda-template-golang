3c0195de76505ef35c289d50697f7f3dbb2a32a9
Javier Telio <javiert@omnidoc.com>
2026-01-16 23:19:59 -0600

    feat(hello): implement name validation and error handling in SayHelloUseCase

--- test/application/use_cases/hello/say_hello_use_case_test.go
+++ test/application/use_cases/hello/say_hello_use_case_test.go
@@ -1,7 +1,8 @@
 package hello
 
 import (
-	"fmt"
+	"errors"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -13,6 +14,7 @@ type SayHelloUseCaseTestSuite struct {
 	suite.Suite
 	name   string
 	result string
+	err    error
 }
 
 func TestSayHelloUseCaseTestSuite(t *testing.T) {
@@ -21,28 +23,174 @@ func TestSayHelloUseCaseTestSuite(t *testing.T) {
 }
 
 func (suite *SayHelloUseCaseTestSuite) SetupTest() {
-	suite.result = "Hello Joe!"
+	suite.name = ""
+	suite.result = ""
+	suite.err = nil
 }
 
-func (suite *SayHelloUseCaseTestSuite) givenName(name string) {
+func (suite *SayHelloUseCaseTestSuite) givenValidName(name string) {
+	suite.name = name
+}
+
+func (suite *SayHelloUseCaseTestSuite) givenEmptyName() {
+	suite.name = ""
+}
+
+func (suite *SayHelloUseCaseTestSuite) givenNameWithSpaces() {
+	suite.name = "   "
+}
+
+func (suite *SayHelloUseCaseTestSuite) givenLongName(length int) {
+	suite.name = strings.Repeat("a", length)
+}
+
+func (suite *SayHelloUseCaseTestSuite) givenInvalidName(name string) {
 	suite.name = name
 }
 
 func (suite *SayHelloUseCaseTestSuite) whenSayHelloUseCaseIsCalled() {
-	suite.result = hello.SayHelloUseCase(suite.name)
+	suite.result, suite.err = hello.SayHelloUseCase(suite.name)
 }
 
-func (suite *SayHelloUseCaseTestSuite) thenSayHello() {
-	suite.Equal(suite.result, fmt.Sprintf("Hello %s!", suite.name))
+func (suite *SayHelloUseCaseTestSuite) thenShouldReturnGreeting(expected string) {
+	suite.NoError(suite.err)
+	suite.Equal(expected, suite.result)
 }
 
-func (suite *SayHelloUseCaseTestSuite) TestSayHelloUseCase() {
+func (suite *SayHelloUseCaseTestSuite) thenShouldReturnError(expectedErr error) {
+	suite.Error(suite.err)
+	suite.True(errors.Is(suite.err, expectedErr))
+	suite.Empty(suite.result)
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithValidName() {
 	// Given
-	suite.givenName("Joe")
+	suite.givenValidName("Joe")
 
 	// When
 	suite.whenSayHelloUseCaseIsCalled()
 
 	// Then
-	suite.thenSayHello()
+	suite.thenShouldReturnGreeting("Hello Joe!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithNameContainingSpaces() {
+	// Given
+	suite.givenValidName("John Doe")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnGreeting("Hello John Doe!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithInternationalCharacters() {
+	// Given
+	suite.givenValidName("José María")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnGreeting("Hello José María!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithHyphen() {
+	// Given
+	suite.givenValidName("Mary-Jane")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnGreeting("Hello Mary-Jane!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithApostrophe() {
+	// Given
+	suite.givenValidName("O'Brien")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnGreeting("Hello O'Brien!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithEmptyName_ShouldDefaultToWorld() {
+	// Given
+	suite.givenEmptyName()
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnGreeting("Hello world!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithOnlySpaces_ShouldDefaultToWorld() {
+	// Given
+	suite.givenNameWithSpaces()
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnGreeting("Hello world!")
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithNameTooLong_ShouldReturnError() {
+	// Given
+	suite.givenLongName(101)
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnError(hello.ErrNameTooLong)
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithInvalidCharacters_ScriptTag_ShouldReturnError() {
+	// Given
+	suite.givenInvalidName("<script>alert('xss')</script>")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnError(hello.ErrInvalidCharacters)
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithInvalidCharacters_SQLInjection_ShouldReturnError() {
+	// Given
+	suite.givenInvalidName("'; DROP TABLE users--")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnError(hello.ErrInvalidCharacters)
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithInvalidCharacters_SpecialSymbols_ShouldReturnError() {
+	// Given
+	suite.givenInvalidName("John@Doe")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnError(hello.ErrInvalidCharacters)
+}
+
+func (suite *SayHelloUseCaseTestSuite) TestSayHelloWithInvalidCharacters_PathTraversal_ShouldReturnError() {
+	// Given
+	suite.givenInvalidName("../../../etc/passwd")
+
+	// When
+	suite.whenSayHelloUseCaseIsCalled()
+
+	// Then
+	suite.thenShouldReturnError(hello.ErrInvalidCharacters)
 }
